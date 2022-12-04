2fd340e7bf02dd2e11da8498f28430208cbf87d9
Karina <118188813+SpicyScalpel@users.noreply.github.com>
2022-12-04 20:18:57 +0300

    Update Index_massi_tela.py

--- Index_massi_tela.py
+++ Index_massi_tela.py
@@ -1,35 +1,37 @@
-print("Tere! Olen sinu uus sõber — Python!")
+print("Tere! Olen sinu uus sÃµber â€” Python!")
 name=input("Mis on sinu nimi on?")
 print("Oi kui ilus nimi!", name)
-indeks=input(name + "Kas leian Sinu keha indeksi? 0-ei, 1-jah=>")
+indeks=input(name + ", Kas leian Sinu keha indeksi? 0-ei, 1-jah=>") #kirjuta pikkus murdarvuna
+if indeks=="0":
+  print("Kahju! See on vÃ¤ga kasulik info!")
+  print()
+  print("Kohtimiseni, "+name+"! Igavesti Sinu, Python!")
 if indeks=="1":
   try:
-      pikkus=int(input("Pikkus: "))
+      pikkus=float(input("Pikkus: "))
   except:
       pikkus=175
       print("Error, siis pikkus= 175")
   try:
-      mass=int(input("Mass: "))
+      mass=float(input("Mass: "))
   except:
       mass=55
       print("Error, siis mass= 55")
-indeks=mass/pikkus
-print(name + "Sinu keha indeks on:", round(indeks,1))
-if indeks<16:
-  print("Tervisele ohtlik alakaal") 
-elif indeks>=16 and indeks<19:
-  print("Alakaal")
-elif indeks>=19 and indeks<25:
-  print("Normaalkaal")
-elif indeks>=25 and indeks<30:
-  print("Ülekaal")
-elif indeks>=30 and indeks<35:
-  print("Rasvimune")
-elif indeks>=35 and indeks<40:
-  print("Tugev rasvamine")
-elif indeks>=40:
-  print("Tervisele ohtlik rasvamine")
-else:
-  print("Kahju! See on väga kasulik info!")
-print()
-print("Kohtimiseni,"+name+"! Igavesti Sinu, Python!")
+  indeks=mass/(pikkus)**2
+  print(name + ", sinu keha indeks on:", round(indeks,1))
+  if indeks<16:
+    print("Tervisele ohtlik alakaal") 
+  elif indeks>=16 and indeks<19:
+    print("Alakaal")
+  elif indeks>=19 and indeks<25:
+    print("Normaalkaal")
+  elif indeks>=25 and indeks<30:
+    print("Ãœlekaal")
+  elif indeks>=30 and indeks<35:
+    print("Rasvimune")
+  elif indeks>=35 and indeks<40:
+    print("Tugev rasvamine")
+  elif indeks>=40:
+    print("Tervisele ohtlik rasvamine")
+  else:
+    print()
