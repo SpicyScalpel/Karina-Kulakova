c08c44674a780f364f81b16f01bc2fa3176239af
Karina <118188813+SpicyScalpel@users.noreply.github.com>
2023-04-04 00:37:08 +0300

    Add files via upload

---
+++ Kodutööd/C#2/Snake/Direction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Right,
+        Left
+    }
+}
+    internal class Direction
+    {
+
+    }
